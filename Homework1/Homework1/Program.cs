34af6e49147315ef94b0fce8351ccd60646f9621
庭嘉 張 <52713rose@gmail.com>
2019-03-31 11:21:23 +0800

    Hi

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(number(2, 1));
+            Console.WriteLine(number(2, 3));
+            Console.WriteLine(number(2, 2));
+            Console.ReadLine();
+        }
+
+        private static int number(int x, int y)
+        {
+            return x == y ? (x + y) * 3 : x + y;
+        }
+    }
+}
